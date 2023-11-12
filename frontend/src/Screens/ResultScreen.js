8759eafe7b3eccf9bb4d64966ce1c388c5526efe
astraikis <astraikis.jackson@gmail.com>
2023-11-11 20:49:59 -0500

    added ingredient list and flagged ingredients

---
+++ frontend/src/Screens/ResultScreen.js
@@ -0,0 +1,117 @@
+import { useEffect, useState } from "react";
+import { useParams, useNavigate } from "react-router-dom";
+import axios, { all } from "axios";
+
+function ResultScreen() {
+    const [productName, setProductName] = useState("");
+    const [productImageUrl, setProductImageUrl] = useState("");
+    const [allergenList, setAllergenList] = useState([]);
+    const [ingredientList, setIngredientList] = useState([]);
+    const [flaggedIngredientList, setFlaggedIngredientList] = useState([]);
+    const [flaggedToRef, setFlaggedToRef] = useState({});
+
+    let { upc } = useParams();
+    const navigate = useNavigate();
+
+    const getProductInfo = () => {
+        axios.get(`https://world.openfoodfacts.org/api/v2/product/${upc}.json`).then((response) => {
+            setProductName(response.data.product.product_name);
+            setProductImageUrl(response.data.product.selected_images.front.display.en);
+
+            const allergens = response.data.product.allergens_hierarchy;
+            let allergensArr = [];
+            allergens.forEach((allergen, _) => {
+                const allergenSplit = allergen.split(":");
+                allergensArr.push(allergenSplit[allergenSplit.length - 1]);
+            });
+            setAllergenList(allergensArr);
+            
+            const ingredients = response.data.product.ingredients;
+            let ingredientArr = [];
+            ingredients.forEach((ingredient, _) => {
+                ingredientArr.push(ingredient.text);
+            });
+            setIngredientList(ingredientArr);
+        })
+    }
+
+    const getFlaggedIngredients = () => {
+        axios.get("http://localhost:8080/ingredients").then((response) => {
+            const ingredients = response.data;
+            let ingredientArr = [];
+            let flaggedToRefTemp = {};
+            ingredients.forEach((ingredient, _) => {
+                if (ingredient.name !== null) {
+                    flaggedToRefTemp[ingredient.name] = ingredient.reference;
+                    ingredientArr.push(ingredient.name.toLowerCase());
+                }
+            });
+            setFlaggedToRef(flaggedToRefTemp);
+            setFlaggedIngredientList(ingredientArr);
+        })
+    }
+
+    useEffect(() => {
+        getProductInfo();
+        getFlaggedIngredients();
+    }, [navigate]);
+
+    return (
+        <div>
+            <div className="container py-5">
+                <div className="row">
+                <div className="col-3">
+                    { productImageUrl ?
+                        <img src={productImageUrl} alt={"image of " + productName} className="rounded" style={{width: "250px"}} />
+                    :
+                        <div></div>
+                    }
+                </div>
+                    <div className="col">
+                        <h1 className="mb-4">{productName}</h1>
+
+                        <p className="fs-4 text-body-secondary mb-4">
+                            { ingredientList.map((ingredient, i) => {
+                                const flagged = flaggedIngredientList.includes(ingredient);
+                                const reference = flaggedToRef[ingredient.toLowerCase()];
+                                return (
+                                    flagged ?
+                                        <span>
+                                            <a href={reference} target="_blank" rel="noopener noreferrer">
+                                                <span key={ingredient} className="text-danger text-decoration-underline">
+                                                    { ingredient.toLowerCase() }
+                                                    <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-box-arrow-up-right" viewBox="0 0 16 16">
+                                                        <path fill-rule="evenodd" d="M8.636 3.5a.5.5 0 0 0-.5-.5H1.5A1.5 1.5 0 0 0 0 4.5v10A1.5 1.5 0 0 0 1.5 16h10a1.5 1.5 0 0 0 1.5-1.5V7.864a.5.5 0 0 0-1 0V14.5a.5.5 0 0 1-.5.5h-10a.5.5 0 0 1-.5-.5v-10a.5.5 0 0 1 .5-.5h6.636a.5.5 0 0 0 .5-.5z"/>
+                                                        <path fill-rule="evenodd" d="M16 .5a.5.5 0 0 0-.5-.5h-5a.5.5 0 0 0 0 1h3.793L6.146 9.146a.5.5 0 1 0 .708.708L15 1.707V5.5a.5.5 0 0 0 1 0v-5z"/>
+                                                    </svg>
+                                                </span>
+                                            </a>
+                                            { i < ingredientList.length - 1 && <span className="text-body-secondary">, </span>}
+                                        </span>
+                                    :
+                                        <span key={ingredient}>
+                                            { ingredient.toLowerCase() }
+                                            { i < ingredientList.length - 1 && ", "}
+                                        </span>
+                                );
+                            })}
+                        </p>
+
+                        <p className="fs-5 text-body-secondary mb-5">
+                            Allergens: { allergenList.map((allergen, i) => {
+                                return (
+                                    <span key={allergen}>
+                                        { allergen.toLowerCase() }
+                                        { i < allergenList.length - 1 && ", "}
+                                    </span>
+                                );
+                            })}
+                        </p>
+                    </div>
+                </div>
+            </div>
+        </div>
+    );
+}
+
+export default ResultScreen;
--- frontend/src/components/Scanner.js
+++ frontend/src/components/Scanner.js
@@ -1,8 +1,11 @@
 import { useEffect, useState } from 'react';
+import { useNavigate } from 'react-router-dom';
 import { Html5QrcodeScanner } from 'html5-qrcode';
 
 function Scanner() {
     const [scanResult, setScanResult] = useState(null);
+
+    const navigate = useNavigate();
 
     useEffect(() => {
         const scanner = new Html5QrcodeScanner("reader", {
@@ -16,23 +19,23 @@ function Scanner() {
         scanner.render(success, error);
     
         function success(result) {
-        scanner.clear();
-        setScanResult(result);
+            scanner.clear();
+            setScanResult(result);
+            navigate(`/product/${result}`);
         }
     
         function error(err) {
-        console.warn(err);
+            console.warn(err);
         }
     }, []);
 
     return (
-        <div className="App">
+        <div>
             { scanResult 
                 ? <div>success: <a href={`https://world.openfoodfacts.org/api/v2/product/${scanResult}.json`}>results</a></div>
                 : <div id="reader"></div>
             }
-            <div id="reader">
-            </div>
+            <div id="reader"></div>
         </div>
     );
 }
