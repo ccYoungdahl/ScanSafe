99768795f8c15c4d2ef73d2c58305208d951e774
amissingreno <142545854+amissingreno@users.noreply.github.com>
2023-12-04 11:27:31 -0500

    Influencer routes added!

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,6 +3,11 @@ import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import ScanScreen from "./Screens/ScanScreen";
 import ResultScreen from "./Screens/ResultScreen";
 import YourIngredientsScreen from "./Screens/YourIngredientsScreen";
+import InfluencerDashboard from "./Screens/InfluencerDashboard";
+import AlternativeProductForm from "./Screens/AlternativeProductForm";
+import ProposedIngredientForm from './Screens/ProposedIngredientForm';
+import UpdateAlternativeProduct from './Screens/UpdateAlternativeProduct';
+import UpdateProposedIngredient from './Screens/UpdateProposedIngredient';
 
 import Navbar from "./components/Navbar";
 
@@ -16,6 +21,11 @@ function App() {
           <Route path=":upc" element={<ResultScreen />} />
         </Route>
         <Route path="/your-ingredients" element={<YourIngredientsScreen />} />
+        <Route path="InfluencerDashboard" element={<InfluencerDashboard />} />
+        <Route path="AlternativeProductForm" element={<AlternativeProductForm />} />
+        <Route path="ProposedIngredientForm" element={<ProposedIngredientForm />} />
+        <Route path="UpdateAlternativeProduct/:id" element={<UpdateAlternativeProduct />} />
+        <Route path="UpdateProposedIngredient/:id" element={<UpdateProposedIngredient /> } />
       </Routes>
     </Router>
   );
